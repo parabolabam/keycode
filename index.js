567407391a3c98fb13ccc87bc8e6c6c03d118ba5
Vlad Sorokin <homiecrewtm@gmail.com>
2020-10-24 16:19:48 +0600

    feat: change layout so it works on mobile devices

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 'use strict';
 
 const keyCodeContainer = document.querySelector('#keyCodeContainer');
+const root = document.querySelector('#root');
 
 const eventKey = document.querySelector('#eventKey');
-const eventLocation = document.querySelector('#eventLocation');
 const eventWhich = document.querySelector('#eventWhich');
 const eventCode = document.querySelector('#eventCode');
+const overlay = document.querySelector('#overlay');
 
 window.addEventListener('keyup', function onKeyDown(e) {
+    overlay?.remove();
+    root.classList.remove('root__invisible');
     keyCodeContainer.innerHTML = null;
     keyCodeContainer.innerText = e.keyCode;
     document.title = e.code;
 
     insertInfoIntoCardBody(eventKey, e.key);
-    insertInfoIntoCardBody(eventLocation, e.location);
     insertInfoIntoCardBody(eventWhich, e.which);
     insertInfoIntoCardBody(eventCode, e.code);
 });
