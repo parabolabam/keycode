d70642db165acc4dc907a93b079b4776afade2cf
Vlad Sorokin <homiecrewtm@gmail.com>
2020-10-24 12:07:50 +0600

    feat: add styles

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 'use strict';
 
 const keyCodeContainer = document.querySelector('#keyCodeContainer');
-const codeContainer = document.querySelector('#codeContainer');
+
+const eventKey = document.querySelector('#eventKey');
+const eventLocation = document.querySelector('#eventLocation');
+const eventWhich = document.querySelector('#eventWhich');
+const eventCode = document.querySelector('#eventCode');
 
 window.addEventListener('keyup', function onKeyDown(e) {
+    keyCodeContainer.innerHTML = null;
     keyCodeContainer.innerText = e.keyCode;
-    codeContainer.innerText = e.code;
-    document.title = e.keyCode;
+    document.title = e.code;
+
+    insertInfoIntoCardBody(eventKey, e.key);
+    insertInfoIntoCardBody(eventLocation, e.location);
+    insertInfoIntoCardBody(eventWhich, e.which);
+    insertInfoIntoCardBody(eventCode, e.code);
 });
+
+function insertInfoIntoCardBody(cardContainer, info) {
+    const infoContainer = cardContainer.querySelector('.card__body');
+    infoContainer.innerText = info;
+}
+
