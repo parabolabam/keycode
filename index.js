2f761184dc39854a6d7f9dc46936531c8b5f4779
Vlad Sorokin <homiecrewtm@gmail.com>
2020-10-24 17:18:31 +0600

    feat: add mobile message

--- index.js
+++ index.js
@@ -1,27 +1,45 @@
-'use strict';
+"use strict";
 
-const keyCodeContainer = document.querySelector('#keyCodeContainer');
-const root = document.querySelector('#root');
+const mobileDeviceText = `
+  looks like you are from mobile device, 
+  we don't support mobile device  as
+  there are no keycodes of touch events 
+`;
 
-const eventKey = document.querySelector('#eventKey');
-const eventWhich = document.querySelector('#eventWhich');
-const eventCode = document.querySelector('#eventCode');
-const overlay = document.querySelector('#overlay');
+const keyCodeContainer = document.querySelector("#keyCodeContainer");
+const overlayContent = document.querySelector("#overlay-content");
+const root = document.querySelector("#root");
 
-window.addEventListener('keyup', function onKeyDown(e) {
-    overlay?.remove();
-    root.classList.remove('root__invisible');
-    keyCodeContainer.innerHTML = null;
-    keyCodeContainer.innerText = e.keyCode;
-    document.title = e.code;
+const eventKey = document.querySelector("#eventKey");
+const eventWhich = document.querySelector("#eventWhich");
+const eventCode = document.querySelector("#eventCode");
+const overlay = document.querySelector("#overlay");
 
-    insertInfoIntoCardBody(eventKey, e.key);
-    insertInfoIntoCardBody(eventWhich, e.which);
-    insertInfoIntoCardBody(eventCode, e.code);
-});
+window.addEventListener("keyup", onKeyDown, false);
+window.addEventListener("touchend", onTouchEnd, false);
 
 function insertInfoIntoCardBody(cardContainer, info) {
-    const infoContainer = cardContainer.querySelector('.card__body');
-    infoContainer.innerText = info;
+  const infoContainer = cardContainer.querySelector(".card__body");
+  infoContainer.innerText = info;
 }
 
+function onKeyDown(e) {
+  overlay?.remove();
+  root.classList.remove("root__invisible");
+  keyCodeContainer.innerHTML = null;
+  keyCodeContainer.innerText = e.keyCode;
+  document.title = e.code;
+
+  insertInfoIntoCardBody(eventKey, e.key);
+  insertInfoIntoCardBody(eventWhich, e.which);
+  insertInfoIntoCardBody(eventCode, e.code);
+}
+
+function onTouchEnd() {
+  const emojiContainer = document.createElement("div", {
+      classList: ['emoji-container--flexed']
+  });
+  emojiContainer.innerText = "🐱‍🚀";
+  overlayContent.innerText = mobileDeviceText;
+  overlayContent.appendChild(emojiContainer);
+}
